db8f470453220ca15aea8e848527bd65e734954c
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-12-15 22:39:52 +1100

    stored the secrets in ebvironmental variables and learned roled-based authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token provided");
   try {
-    const decoded = jwt.verify(token, config.get("jswtPrivateKey"));
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.user = decoded;
     next();
   } catch (exception) {
