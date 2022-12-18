5265ad9675be3a0c6f002aacaf76d3a53a2a208f
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-12-18 23:35:35 +1100

    extracted routers

--- index.js
+++ index.js
@@ -1,60 +1,17 @@
-require("express-async-errors");
 const winston = require("winston");
-require("winston-mongodb");
-const error = require("./middleware/error");
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectId")(Joi);
-const mongoose = require("mongoose");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 
-winston.handledExceptions(
-  new winston.transports.File({ finename: "uncaughtExceptions.log" })
-);
+require("./startup/logging");
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-});
+// const p = Promise.reject(new Error("Something failed miserably"));
+// p.then(() => console.log("Done"));
 
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost/vidly",
-    level: "info",
-  })
-);
-
-const p = Promise.reject(new Error("Something failed miserably"));
-p.then(() => console.log("Done"));
-
-throw new Error("Could not get genres.");
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
-
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
+// throw new Error("Could not get genres.");
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
